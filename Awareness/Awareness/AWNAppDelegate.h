8b0ff6f2726d6f51ad3ededc26c52d25831a11bb
Jeff Kibuule <jeff.kibuule@outlook.com>
2014-08-18 09:50:07 -0500

    Initial project commit

---
+++ Awareness/Awareness/AWNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AWNAppDelegate.h
+//  Awareness
+//
+//  Created by Jeff Kibuule on 8/18/14.
+//  Copyright (c) 2014 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AWNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Awareness/Awareness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Awareness
+//
+//  Created by Jeff Kibuule on 8/18/14.
+//  Copyright (c) 2014 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AWNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AWNAppDelegate class]));
+    }
+}
